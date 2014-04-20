64a06f885b5c2e805763b40791bf2ac05f893e4b
Maxim Tsaplin <maxim.tsaplin@gmail.com>
2014-04-21 01:51:57 +0300

    Add validation for reservation overlapping.

--- app/models/table_reservation.rb
+++ app/models/table_reservation.rb
@@ -6,6 +6,7 @@ class TableReservation < ActiveRecord::Base
 
   validate :ended_at_greater_than_started_at
   validate :reservation_is_in_future
+  validate :reservation_overlapping
 
   private
 
@@ -19,6 +20,18 @@ class TableReservation < ActiveRecord::Base
     if self.started_at && (self.started_at < DateTime.current)
       self.errors.add(:started_at, "can't be in the past")
     end
+  end
+
+  def reservation_overlapping
+    self.errors.add(:base, "Dates overlap existing reservations") if overlaps?
+  end
+
+  def overlaps?
+    TableReservation.where("table_number = ?", self.table_number).
+                     where("(:started_at >= started_at AND :started_at < ended_at) OR
+                            (:ended_at > started_at AND :ended_at < ended_at) OR
+                            (:started_at <= started_at AND :ended_at >= ended_at)",
+            {ended_at: self.ended_at.to_s(:db), started_at: self.started_at.to_s(:db)}).any?
   end
 
 end
--- spec/models/table_reservation_spec.rb
+++ spec/models/table_reservation_spec.rb
@@ -32,6 +32,77 @@ describe TableReservation do
         expect(table_reservation.errors.full_messages).to be_eql ["Started at can't be in the past"]
       end
     end
+
+    shared_context "not valid" do
+
+      it "is not valid" do
+        expect(table_reservation).to_not be_valid
+      end
+
+      it "adds validation error message" do
+        table_reservation.valid?
+        expect(table_reservation.errors.full_messages).to be_eql error_message
+      end
+
+    end
+
+    describe "#reservation_overlapping" do
+
+      context "when table is the same" do
+        let!(:table_reservation1) { create :table_reservation, table_number: 1, started_at: DateTime.current + 10.hours, ended_at: DateTime.current + 14.hours }
+        let(:table_reservation) { build :table_reservation, table_number: 1, started_at: started_at, ended_at: ended_at }
+        let(:error_message) { ["Dates overlap existing reservations"] }
+
+        context "when only end date overlaps" do
+          let(:started_at) { DateTime.current + 5.hour }
+          let(:ended_at) { DateTime.current + 11.hour }
+
+          it_behaves_like "not valid"
+        end
+
+        context "when only start date overlaps" do
+          let(:started_at) { DateTime.current + 11.hour }
+          let(:ended_at) { DateTime.current + 15.hour }
+
+          it_behaves_like "not valid"
+        end
+
+        context "when bound is greater than existing reservation" do
+          let(:started_at) { DateTime.current + 9.hour }
+          let(:ended_at) { DateTime.current + 15.hour }
+
+          it_behaves_like "not valid"
+        end
+      end
+
+      context "when table is different" do
+        let!(:table_reservation) { create :table_reservation, table_number: 1, started_at: DateTime.current + 10.hours, ended_at: DateTime.current + 14.hours }
+        let!(:table_reservation1) { build :table_reservation, table_number: 2, started_at: started_at, ended_at: ended_at }
+
+        subject { table_reservation1 }
+
+        context "when only end date overlaps" do
+          let(:started_at) { DateTime.current + 5.hour }
+          let(:ended_at) { DateTime.current + 11.hour }
+
+          it { should be_valid }
+        end
+
+        context "when only start date overlaps" do
+          let(:started_at) { DateTime.current + 11.hour }
+          let(:ended_at) { DateTime.current + 15.hour }
+
+          it { should be_valid }
+        end
+
+        context "when bound is greater than existing reservation" do
+          let(:started_at) { DateTime.current + 9.hour }
+          let(:ended_at) { DateTime.current + 15.hour }
+
+          it { should be_valid }
+        end
+      end
+    end
   end
 
 end
